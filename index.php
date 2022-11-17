6a5665a72241890024b8e2ba49687d23cae85739
soufianeargane <112890451+soufianeargane@users.noreply.github.com>
2022-11-17 23:31:44 +0100

    just added maquete

--- index.php
+++ index.php
@@ -111,22 +111,37 @@ if (!empty($_SESSION["id"])) {
             </button>
         </div>
     </section>
-    <div class="container p-2 bg-black  flex mx-auto">
-        <div class="bg-blue-300 p-2 flex mr-2 items-center">
+    <div class="container p-2   flex mx-auto">
+        <div style="box-shadow: -1px 10px 15px -3px rgba(0,0,0,0.27);" class=" p-2 flex mr-2 items-center">
             <div class="p-2">
                 <h3 class="italic ">Total users</h3>
-                <p class="text-2xl">15</p>
+                <p class="text-2xl">
+                    <?php
+                    $query = "SELECT * from tb_user";
+                    $data = mysqli_query($conn, $query);
+                    $user_num = mysqli_num_rows($data);
+                    echo $user_num;
+                    ?>
+                </p>
             </div>
+
             <span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-20  h-10">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                 </svg>
             </span>
         </div>
-        <div class="bg-blue-300 p-2  flex items-center">
+        <div style="box-shadow: -1px 10px 15px -3px rgba(0,0,0,0.27);" class="drop-shadow-2xl p-2  flex items-center">
             <div class="p-2">
-                <h3>Total users</h3>
-                <p class="text-2xl">15</p>
+                <h3 class="italic ">Total products</h3>
+                <p class="text-2xl">
+                    <?php
+                    $query = "SELECT * from products";
+                    $data = mysqli_query($conn, $query);
+                    $user_num = mysqli_num_rows($data);
+                    echo $user_num;
+                    ?>
+                </p>
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-20  h-10">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
